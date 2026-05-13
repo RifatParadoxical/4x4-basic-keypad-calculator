0dc005422aed4cfb5a9e0622b5a0673f5f274408
Muhammad Rifat <137922327+RifatParadoxical@users.noreply.github.com>
2026-05-13 11:41:05 +0600

    Implement division by zero error handling
    Added error handling for division by zero and updated calculation logic.

--- keypad-basic-calculator.ino
+++ keypad-basic-calculator.ino
@@ -66,15 +66,41 @@ void loop()
       }
     }
     else if(key == 'A'|| key == 'B'|| key == 'C' || key == 'D'){
-      secondnum = true;
-      if(key == 'A') op = '+';
-      else if(key == 'B') op = '-';
-      else if(key == 'C') op = '*';
-      else op = '/';
+      if(num1 != "" && num2 != ""){
+        float n1 = num1.toFloat();
+      float n2 = num2.toFloat();
+      float result = 0;
+      if(op == '+') result = n1+n2;
+      else if(op == '-') result = n1-n2;
+      else if(op == '*') result = n1*n2;
+      else if(op == '/'){
+        if(n2 == 0){
+          lcd.clear();
+          lcd.print("Error: /0");
+          delay(2000);
+          lcd.clear();
+          lcd.print("0");
+          num1 = ""; num2 = ""; op = 0; secondnum = false;
+          return;
+        }
+        else result = n1/n2;
+      }
+        lcd.clear();
+        num1 = String(result);
+        lcd.print(num1);
+        num2 = ""; secondnum = true;
+      }
+      else{
+      	secondnum = true;
+      	if(key == 'A') op = '+';
+      	else if(key == 'B') op = '-';
+      	else if(key == 'C') op = '*';
+      	else op = '/';
       
-      lcd.setCursor(0,0);
-      lcd.print(num1);
-      lcd.print(op);
+      	lcd.setCursor(0,0);
+      	lcd.print(num1);
+      	lcd.print(op);
+      }
     }
     else if(key == '#' && num1 != "" && num2 != ""){
       float n1 = num1.toFloat();
